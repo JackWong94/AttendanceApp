97bc8b80fc40308c2022c31733546db8dc0abb05
JackWong94 <w.yuheng94@gmail.com>
2025-09-21 17:04:54 +0800

    Deploy Flutter web app to CKHardware (ckhardware)

--- ckhardware/flutter_bootstrap.js
+++ ckhardware/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "455779211"
+    serviceWorkerVersion: "446759148"
   }
 });
