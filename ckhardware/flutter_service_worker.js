81cc75a5a21143a763eef625f88f3735fdbc0072
JackWong94 <w.yuheng94@gmail.com>
2025-10-09 23:36:09 +0800

    Deploy Flutter web app to CKHardware (ckhardware)

--- ckhardware/flutter_service_worker.js
+++ ckhardware/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d9a61ef8dc0c923de3d2e42cb4580c4d
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a26d540cbb9bc5dba61f03da21f61275",
+"flutter_bootstrap.js": "8ca8e06c8ae61b24e6859fd99e6a317b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
