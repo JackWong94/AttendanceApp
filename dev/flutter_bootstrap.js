a9d53aa27d078f0d2eead1062916ee27867c8430
JackWong94 <w.yuheng94@gmail.com>
2025-10-16 00:04:28 +0800

    Deploy Flutter web app to Development (dev)

--- dev/flutter_bootstrap.js
+++ dev/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "536783712"
+    serviceWorkerVersion: "1373077277"
   }
 });
