67a368e0c8389d0bcb4353fd63b2135ff69935e9
JackWong94 <w.yuheng94@gmail.com>
2026-02-17 09:26:39 +0800

    Release V1.0.3

--- attendanceapp/lib/pages/login_user_page.dart
+++ attendanceapp/lib/pages/login_user_page.dart
@@ -35,7 +35,7 @@ class LoginUserPage extends StatefulWidget {
 
 class _LoginUserPageState extends State<LoginUserPage>
     with RouteAware, SingleTickerProviderStateMixin {
-  static const String appVersion = "Version: 1.0.2";
+  static const String appVersion = "Version: 1.0.3";
   final CameraService _cameraService = CameraService.instance;
   final AttendanceService _attendanceService = AttendanceService();
   final ScanOverlayManager _overlayManager = ScanOverlayManager();
