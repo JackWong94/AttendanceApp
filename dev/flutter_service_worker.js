537d76929ec339e3dd9eea86af2d6f2ad3d1e725
JackWong94 <w.yuheng94@gmail.com>
2026-06-01 22:14:59 +0800

    Deploy Flutter web app to Development (dev)

--- dev/flutter_service_worker.js
+++ dev/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d9a61ef8dc0c923de3d2e42cb4580c4d
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a6e4ed15b236473546a93c2caa4efee2",
+"flutter_bootstrap.js": "00daac8996a1ba8e09accdb3acd862a1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
